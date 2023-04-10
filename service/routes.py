7ad0fb9e3716bc446bee27bc1cec977bc63baf46
TS-Kiran <tskiran213@gmail.com>
2023-04-09 23:47:59 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -89,7 +90,6 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-    
 
 
 ######################################################################
